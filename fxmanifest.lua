afd4997295a5c35694aacc5b61c9e3be090bc095
Isneaxt <109867101+Isneaxt@users.noreply.github.com>
2022-07-23 20:09:51 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+game 'gta5'
+
+fx_version 'adamant'
+
+version '1.0.0' -- DON'T TOUCH THIS
+
+client_script 'main.lua'
+
+server_script 'version_check.lua'
+
+ui_page 'ui.html'
+
+files {
+   'ui.html'
+}
+
+author '𝐼𝓈𝓃𝑒𝒶𝓍𝓉#2317'
